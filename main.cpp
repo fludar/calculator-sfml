593fd7e01f5088bec6e1a20c51e494cb7be4862e
bogdy <fratebogdy@gmail.com>
2025-07-26 12:54:47 +0300

    added button text array

--- main.cpp
+++ main.cpp
@@ -26,17 +26,36 @@ int main()
     sf::Color btnColor(146, 175, 215, 255);
     sf::Color secondaryBtnColor(90, 118, 132, 255);
 
+	std::vector<sf::Text> buttonTexts;
+	sf::Color buttonTextColor(255, 255, 255, 255);
+
 	float posY = 100.0f;
     for (int i = 0; i < 4; ++i)
     {
 
-        for (int i = 0; i < 4; ++i)
+        for (int j = 0; j < 4; ++j)
         {
             sf::RectangleShape button(buttonSize);
             button.setFillColor(btnColor);
-            if (i == 3) button.setFillColor(secondaryBtnColor);
-            button.setPosition(sf::Vector2f(padding.x + i * (buttonSize.x + padding.x), posY + padding.y));
+            if (j == 3) button.setFillColor(secondaryBtnColor);
+            button.setPosition(sf::Vector2f(padding.x + j * (buttonSize.x + padding.x), posY + padding.y));
             buttons.push_back(button);
+        }
+        posY = posY + 75.0f;
+    }
+
+    posY = 100.0f;
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            sf::Text buttonText(font);
+            buttonText.setCharacterSize(18);
+            buttonText.setStyle(sf::Text::Regular);
+            buttonText.setFillColor(buttonTextColor);
+            buttonText.setString(std::to_string(i + 1));
+            buttonText.setPosition(sf::Vector2f(padding.x + j * (buttonSize.x + padding.x) + buttonSize.x / 2 - 5, posY + padding.y + buttonSize.y / 2 - 10));
+            buttonTexts.push_back(buttonText);
         }
         posY = posY + 75.0f;
     }
@@ -65,6 +84,10 @@ int main()
         for (const auto& button : buttons)
         {
             window.draw(button);
+        }
+        for (const auto& buttonText : buttonTexts)
+        {
+            window.draw(buttonText);
         }
         window.display();
     }
